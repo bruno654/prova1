c631942f38de287b20e57b01f64c3cc15208f2cb
bruno654 <67012261+bruno654@users.noreply.github.com>
2023-10-17 23:41:29 +0000

    fix: testando

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import Banner  from "./components/Banner";
-import Courses from "./components/Courses";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-
-
-export default function Home() {
-  return (
-    <>
-      <Header />
-      <Banner />
-      <Courses />
-      <Footer />
-
-    </>
-
+export default function Home(){
+  return(
+    <h1> Teste </h1>
   )
 }
