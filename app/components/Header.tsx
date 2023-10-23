af9b9fc139d851cbb21afbf2a33a80c7e351645d
bruno654 <67012261+bruno654@users.noreply.github.com>
2023-10-23 23:35:20 +0000

    fix: fitclub

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="text-center text-white py-8 border-b border-[#4d4d4d] md:flex itens-center justify-between">
-            <h4>🏫ESCOLA TECNOLÓGICA📘</h4>
+            <h4>🏫FITCLUB📘</h4>
             <nav>
                 <a className="hover:text-[#00e77f]" href="#">
                     Home &nbsp;
